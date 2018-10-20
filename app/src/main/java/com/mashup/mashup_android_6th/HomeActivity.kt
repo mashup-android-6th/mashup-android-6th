8dbefbdc6aad8a67c0a2e631022357da7d637dd9
bigstark <kkjkkdk@gmail.com>
2018-10-20 13:37:38 +0900

    initial commit

---
+++ app/src/main/java/com/mashup/mashup_android_6th/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.mashup.mashup_android_6th
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
