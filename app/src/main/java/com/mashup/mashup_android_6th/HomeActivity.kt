910cb20cd72c813b9dc03a218fcd3e6d29ca1cb9
eldeni <eldeniyenden@gmail.com>
2018-11-17 14:09:55 +0900

    feat: add Github API sample API
    Fixes https://github.com/mashup-android-6th/mashup-android-6th/issues/4

--- app/src/main/java/com/mashup/mashup_android_6th/HomeActivity.kt
+++ app/src/main/java/com/mashup/mashup_android_6th/HomeActivity.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import android.util.Log
 import com.mashup.mashup_android_6th.network.SampleAPI
 import com.mashup.mashup_android_6th.network.SampleRepository
+import com.mashup.mashup_android_6th.network.TempAPI
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-
+import kotlinx.android.synthetic.main.activity_home.*
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 class HomeActivity : AppCompatActivity() {
 
@@ -16,19 +20,41 @@ class HomeActivity : AppCompatActivity() {
     setContentView(R.layout.activity_home)
 
     println("123123")
-    someLogic()
+    func()
+  }
+
+  fun func() {
+    // retrofit
+    val retrofit = Retrofit.Builder()
+      .addCallAdapterFactory(RxJava2CallAdapterFactory.create()) // 1
+      .addConverterFactory(GsonConverterFactory.create()) // 2
+      .baseUrl("https://api.github.com/")
+      .build();
+
+    val api = retrofit.create(TempAPI::class.java)
+    api.getGithub()
+      .subscribeOn(Schedulers.io())
+      .observeOn(AndroidSchedulers.mainThread())
+      .subscribe({ result ->
+        Log.d("Result", "123123 ${result}")
+        tvHello.text = "yoyo ${result.emails_url}"
+      }, {
+        error -> error.printStackTrace()
+      }, {
+        Log.d("Result", "complete")
+      })
   }
 
   fun someLogic() {
-    val api = SampleAPI.create()
-    api.search2()
-      .observeOn(AndroidSchedulers.mainThread())
-      .subscribeOn(Schedulers.io())
-      .subscribe({
-        result -> Log.d("Result", "1123 ${result}")
-      }, {
-        error -> error.printStackTrace()
-      });
+//    val api = SampleAPI.create()
+//    api.search2()
+//      .observeOn(AndroidSchedulers.mainThread())
+//      .subscribeOn(Schedulers.io())
+//      .subscribe({
+//        result -> Log.d("Result", "1123 ${result}")
+//      }, {
+//        error -> error.printStackTrace()
+//      });
 
 //    SampleRepository.search()
 //      .observeOn(AndroidSchedulers.mainThread())
---
+++ app/src/main/java/com/mashup/mashup_android_6th/models/Github.kt
@@ -0,0 +1,7 @@
+package com.mashup.mashup_android_6th.models
+
+data class Github(
+  val emails_url: String,
+  val emojis_url: String,
+  var events_url: String
+)
