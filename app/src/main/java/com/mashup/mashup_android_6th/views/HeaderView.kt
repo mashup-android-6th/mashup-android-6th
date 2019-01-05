99c9e2dac3aa5bc1730281a2d76d6cfd5355a120
Jeongah Shin <jeongah.h.shin@gmail.com>
2019-01-05 13:04:00 +0900

    HeaderView extends constraintlayout added

---
+++ app/src/main/java/com/mashup/mashup_android_6th/views/HeaderView.kt
@@ -0,0 +1,12 @@
+package com.mashup.mashup_android_6th.views
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class HeaderView @JvmOverloads constructor(
+    context : Context,
+    attrs : AttributeSet? = null,
+    defStyleAttr : Int = 0
+): ConstraintLayout(context, attrs, defStyleAttr) {
+}
