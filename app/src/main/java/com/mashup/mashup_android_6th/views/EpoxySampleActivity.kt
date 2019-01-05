645baa8e2ee25fd0df7c8188ca6333f46259d3ce
Jeongah Shin <jeongah.h.shin@gmail.com>
2019-01-05 13:25:07 +0900

    widthmodels() func added

--- app/src/main/java/com/mashup/mashup_android_6th/views/EpoxySampleActivity.kt
+++ app/src/main/java/com/mashup/mashup_android_6th/views/EpoxySampleActivity.kt
@@ -2,11 +2,28 @@ package com.mashup.mashup_android_6th.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.airbnb.epoxy.EpoxyController
+import com.airbnb.epoxy.EpoxyRecyclerView
+import com.mashup.mashup_android_6th.R
+import kotlinx.android.synthetic.main.activity_epoxy_sample.*
 
 class EpoxySampleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_epoxy_sample)
+
+        ervName.withModels {
+            
+        }
     }
+
+}
+
+fun EpoxyRecyclerView.withModels(buildModelsCallback: EpoxyController.() -> Unit) {
+    setControllerAndBuildModels(object : EpoxyController() {
+        override fun buildModels() {
+            buildModelsCallback()
+        }
+    })
 }
