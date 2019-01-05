4e43bbfb99e3a098417cefc5b0e7b38c7787a181
jeongahri <jeongah.h.shin@gmail.com>
2019-01-05 13:50:16 +0900

    2019년 1월 5일 airbnb/epoxy 실습 by 대규느님 (#8)
    * epoxy dependency added
    
    * base layout structure for recyclerview using epoxy; activity and header,footer,item
    
    * HeaderView extends constraintlayout added
    
    * add epoxy annotation to HeaderView; @ModelView
    
    * inflate its own view
    
    * add footer view
    
    * add item view; @TextProp, @AfterPropSet
    
    * widthmodels() func added
    
    * Caution; annotationProcessor to kapt in dependency setting

---
+++ app/src/main/java/com/mashup/mashup_android_6th/views/HeaderView.kt
@@ -0,0 +1,20 @@
+package com.mashup.mashup_android_6th.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.airbnb.epoxy.ModelView
+import com.mashup.mashup_android_6th.R
+
+@ModelView(autoLayout = ModelView.Size.MATCH_WIDTH_WRAP_HEIGHT)
+class HeaderView @JvmOverloads constructor(
+    context : Context,
+    attrs : AttributeSet? = null,
+    defStyleAttr : Int = 0
+): ConstraintLayout(context, attrs, defStyleAttr) {
+
+    init {
+        View.inflate(context, R.layout.viewholder_header, this)
+    }
+}
