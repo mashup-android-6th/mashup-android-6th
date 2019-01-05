a565ccd247283c3e929b5ed61a038a292dfb028a
Jeongah Shin <jeongah.h.shin@gmail.com>
2019-01-05 13:07:21 +0900

    add epoxy annotation to HeaderView; @ModelView

--- app/src/main/java/com/mashup/mashup_android_6th/views/HeaderView.kt
+++ app/src/main/java/com/mashup/mashup_android_6th/views/HeaderView.kt
@@ -3,7 +3,9 @@ package com.mashup.mashup_android_6th.views
 import android.content.Context
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.airbnb.epoxy.ModelView
 
+@ModelView(autoLayout = ModelView.Size.MATCH_WIDTH_WRAP_HEIGHT)
 class HeaderView @JvmOverloads constructor(
     context : Context,
     attrs : AttributeSet? = null,
