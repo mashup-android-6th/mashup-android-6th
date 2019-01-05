da474f3fc32f0de98c28aaff062d1cc5c5d52213
Jeongah Shin <jeongah.h.shin@gmail.com>
2019-01-05 13:01:06 +0900

    base layout structure for recyclerview using epoxy; activity and header,footer,item

---
+++ app/src/main/java/com/mashup/mashup_android_6th/views/EpoxySampleActivity.kt
@@ -0,0 +1,12 @@
+package com.mashup.mashup_android_6th.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EpoxySampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_epoxy_sample)
+    }
+}
